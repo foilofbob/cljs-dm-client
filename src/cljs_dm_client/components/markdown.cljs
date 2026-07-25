84b75f8f234e64e390471b7d11f6ded8054d8d1f
Miles Blount <foilofbob@gmail.com>
2026-07-24 22:50:41 -0500

    Added more markdown extensions

--- src/cljs_dm_client/components/markdown.cljs
+++ src/cljs_dm_client/components/markdown.cljs
@@ -2,15 +2,40 @@
   (:require
    ["markdown-it" :as md]
    ["markdown-it-admon" :as mda]
+   ["markdown-it-classy" :as mdclassy]
+   ["markdown-it-collapsible" :as mdcollapse]
    ["markdown-it-task-lists" :as mdts]
-   ["markdown-it-link-attributes" :as mdla]))
+   ["markdown-it-link-attributes" :as mdla]
+   ["markdown-it-image-figures" :as mdif]
+   #_["markdown-it-plantuml" :as plantuml]
+   #_["markdown-it-regexp" :as regexp]))
 
 (defonce markdown
   (-> (md)
       (.use mda)
       (.use mdts #js {:enabled true})
-      ;; TODO: link attributes doesn't seem to work...
-      (.use mdla #js {:attrs {:target "_blank" :rel "noopener"}})))
+
+      ;; link attributes doesn't seem to work...
+      ;(.use mdla #js {:attrs {:target "_blank" :rel "noopener"}})
+
+      ;; Could be useful? Do we have need for a UML diagrams?
+      ;; https://plantuml.com/class-diagram
+      ;(.use plantuml)
+
+      ;; Could be useful for custom matchers? Perhaps dynamic links? There are custom options, see docs.
+      ;; Also seems to have a broken dependency...
+      ;; https://www.npmjs.com/package/markdown-it-regexp
+      ;;(.use regexp)
+
+      ;; Could be useful for custom tag classes
+      ;; https://www.npmjs.com/package/markdown-it-classy
+      (.use mdclassy)
+
+      ;; Collapsible components
+      ;; https://www.npmjs.com/package/markdown-it-collapsible
+      (.use mdcollapse)
+
+      (.use mdif #js {:lazy true :async true}))) ;; ex.: [![](fig.png)](page.html)
 
 (defn render-markdown [content]
   (.render markdown content))
